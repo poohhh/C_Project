f6d56300f99f5a4b49c25c7bf1eac2ce4788eac6
Nayoung Kim <ny2322@gmail.com>
2021-09-30 20:51:02 +0900

    Initial Commit

---
+++ MyProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MyProject
+//
+//  Created by Nayomi on 2021/09/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
